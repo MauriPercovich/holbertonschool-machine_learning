8bed2a773c91010cede738fb7f38cc7c3d8355ea
mauripercovich <mauriperco@gmail.com>
2023-01-04 14:41:58 -0800

    neuron 2

---
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Module to create a neuron
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    A class that defines a single neuron
+    """
+
+    def __init__(self, nx):
+        """
+        class constructor
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+        """
+        calculates the forward propagation of the neuron
+        """
+        preactivation = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-preactivation))
+        return self.__A
+
+    @property
+    def W(self):
+        """
+        getter function for W
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        getter function for b
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        getter function for W
+        """
+        return self.__A
