c4cedb5cf7ee68c1bc8978a18d6ea043cccfc95a
mauripercovich <mauriperco@gmail.com>
2023-01-04 14:46:01 -0800

    neuron 5

---
+++ supervised_learning/0x01-classification/5-neuron.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""
+Module to create a neuron
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    A class that defines a single neuron
+    """
+
+    def __init__(self, nx):
+        """
+        class constructor
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+        """
+        calculates the forward propagation of the neuron
+        """
+        preactivation = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-preactivation))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        calculates the cost of the model using logistic regression
+        """
+        cost = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = np.sum(cost)
+        cost = - cost / A.shape[1]
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        evaluates the neuron prediction
+        """
+        self.forward_prop(X)
+        prediction = np.where(self.__A >= 0.5, 1, 0)
+        cost = self.cost(Y, self.__A)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """
+        calculates one pass of gradient descent on the neuron
+        """
+        dz = A - Y
+        dw = np.matmul(X, dz.T) / A.shape[1]
+        db = np.sum(dz) / A.shape[1]
+        self.__W = self.__W - alpha * dw.T
+        self.__b = self.__b - alpha * db
+
+    @property
+    def W(self):
+        """
+        getter function for W
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        getter function for b
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        getter function for W
+        """
+        return self.__A
