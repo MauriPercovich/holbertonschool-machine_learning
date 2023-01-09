dcf01857a3869172cba91e67ac7a48b8af50f241
mauripercovich <mauriperco@gmail.com>
2023-01-09 12:51:05 -0800

    fix 1

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -10,6 +10,6 @@ def create_layer(prev, n, activation):
     a function that create layers
     """
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    layer = tf.layers.Dense(units=n, bias_initializer=initializer,
+    layer = tf.layers.Dense(units=n, bias_initializer=init,
                                   activation=activation, name='layer')(prev)
     return layer
